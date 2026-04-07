018eada0f6a2c597a7e6a3cfb042830df2672752
mozo32 <mozorodriguez32@gmail.com>
2026-04-06 18:26:55 -0600

    validacion de la contraseña

--- app/Actions/Fortify/CreateNewUser.php
+++ app/Actions/Fortify/CreateNewUser.php
@@ -29,7 +29,7 @@ class CreateNewUser implements CreatesNewUsers
                 'max:255',
                 Rule::unique(User::class),
             ],
-            'password' => $this->passwordRules(),
+            'password' => ['required', 'string', 'confirmed'],
         ])->validate();
 
         $user = User::create([
